b710fc7fed4cecadf95e804d03776519c60581b1
Mohit Saini <144822812+mohitsaini7407@users.noreply.github.com>
2025-08-17 02:12:07 +0530

    Update Login.jsx

--- src/pages/Authentication/Login.jsx
+++ src/pages/Authentication/Login.jsx
@@ -44,6 +44,11 @@ const Login = () => {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ identifier: id, password }),
       });
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/login`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ identifier: id, password })
+      });
 
       
       if (res.ok) {
